e0e4d6373bbb4be75dbba0620be6be9ab9358902
manikanta700 <20pa1a5455@vishnu.edu.in>
2024-09-30 17:54:21 +0530

    prompt updated

--- assistant.py
+++ assistant.py
@@ -2,11 +2,11 @@ import pickle
 
 from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm,JobRequest
 from livekit.agents.voice_assistant import VoiceAssistant
-from livekit.plugins import deepgram, openai, rag, silero
+from livekit.plugins import deepgram, openai, silero
 from dotenv import load_dotenv
 import psutil 
 load_dotenv()
-annoy_index = rag.annoy.AnnoyIndex.load("vdb_data")  # see build_data.py
+# annoy_index = rag.annoy.AnnoyIndex.load("vdb_data")  # see build_data.py
 
 embeddings_dimension = 1536
 
@@ -15,67 +15,67 @@ embeddings_dimension = 1536
 async def entrypoint(ctx: JobContext):
     await ctx.connect()
     caseid= str(list(ctx.room.remote_participants.values ()) [0].metadata)
-    input_list = caseid.split(",")
-    # print(f"input_list----------------{input_list[0]}")
-    async def _enrich_with_rag(assistant: VoiceAssistant, chat_ctx: llm.ChatContext):
-        # locate the last user message and use it to query the RAG model
-        # to get the most relevant paragraph
-        # then provide that as additional context to the LLM
-        # with open("java.pkl", "rb") as f:
-        #     java_by_uuid = pickle.load(f)
-        # with open("Dbms.pkl", "rb") as f:
-        #     dbms_by_uuid = pickle.load(f)
-        # with open("python.pkl", "rb") as f:
-        #     python_by_uuid = pickle.load(f)
-        # print(f"chat_ctx.messages----------------{chat_ctx.messages}")
-        # user_msg = chat_ctx.messages[-1]
-        # print(f"user_msg----------------{user_msg.content}")
-        # user_embedding = await openai.create_embeddings(
-        #     input=[user_msg.content],
-        #     model="text-embedding-3-small",
-        #     dimensions=embeddings_dimension,
-        # )
+    # input_list = caseid.split(",")
+    # # print(f"input_list----------------{input_list[0]}")
+    # async def _enrich_with_rag(assistant: VoiceAssistant, chat_ctx: llm.ChatContext):
+    #     # locate the last user message and use it to query the RAG model
+    #     # to get the most relevant paragraph
+    #     # then provide that as additional context to the LLM
+    #     # with open("java.pkl", "rb") as f:
+    #     #     java_by_uuid = pickle.load(f)
+    #     # with open("Dbms.pkl", "rb") as f:
+    #     #     dbms_by_uuid = pickle.load(f)
+    #     # with open("python.pkl", "rb") as f:
+    #     #     python_by_uuid = pickle.load(f)
+    #     # print(f"chat_ctx.messages----------------{chat_ctx.messages}")
+    #     # user_msg = chat_ctx.messages[-1]
+    #     # print(f"user_msg----------------{user_msg.content}")
+    #     # user_embedding = await openai.create_embeddings(
+    #     #     input=[user_msg.content],
+    #     #     model="text-embedding-3-small",
+    #     #     dimensions=embeddings_dimension,
+    #     # )
 
-        # result = annoy_index.query(user_embedding[0].embedding, n=1)[0]
-        # print(f"result----------------{result.userdata}")
-        # try:
-        #     python = python_by_uuid[result.userdata]
-        # except:
-        #     python = " "
-        # try:
-        #     java = java_by_uuid[result.userdata]
-        # except:
-        #     java = " "
-        # try:
-        #     dbms = dbms_by_uuid[result.userdata]
-        # except:
+    #     # result = annoy_index.query(user_embedding[0].embedding, n=1)[0]
+    #     # print(f"result----------------{result.userdata}")
+    #     # try:
+    #     #     python = python_by_uuid[result.userdata]
+    #     # except:
+    #     #     python = " "
+    #     # try:
+    #     #     java = java_by_uuid[result.userdata]
+    #     # except:
+    #     #     java = " "
+    #     # try:
+    #     #     dbms = dbms_by_uuid[result.userdata]
+    #     # except:
 
-        #     dbms = " "
-        with open("Technical.pkl", "rb") as f:
-            Technical_by_uuid = pickle.load(f)
-        # user_msg=""
-        # for i in range(len(chat_ctx.messages)):
-        #     user_msg += chat_ctx.messages[i].content
-        user_msg=chat_ctx.messages[-1]
-        # print(f"user_msg----------------{chat_ctx.messages}")
-        # print(f"user_msg----------------{user_msg.content}")
-        user_embedding = await openai.create_embeddings(
-            input=[user_msg.content],
-            model="text-embedding-3-small",
-            dimensions=embeddings_dimension,
-        )
-        result = annoy_index.query(user_embedding[0].embedding, n=1)[0]
-        # print(f"result----------------{result.userdata}")
-        Technical= Technical_by_uuid[result.userdata]
-
-
+    #     #     dbms = " "
+    #     with open("Technical.pkl", "rb") as f:
+    #         Technical_by_uuid = pickle.load(f)
+    #     # user_msg=""
+    #     # for i in range(len(chat_ctx.messages)):
+    #     #     user_msg += chat_ctx.messages[i].content
+    #     user_msg=chat_ctx.messages[-1]
+    #     # print(f"user_msg----------------{chat_ctx.messages}")
+    #     # print(f"user_msg----------------{user_msg.content}")
+    #     user_embedding = await openai.create_embeddings(
+    #         input=[user_msg.content],
+    #         model="text-embedding-3-small",
+    #         dimensions=embeddings_dimension,
+    #     )
+    #     # result = annoy_index.query(user_embedding[0].embedding, n=1)[0]
+    #     # print(f"result----------------{result.userdata}")
+    #     # Technical= Technical_by_uuid[result.userdata]
 
 
-        chat_ctx.messages[-1].content = (
-            "Context:\n" + Technical + "\n\nUser Question: " + chat_ctx.messages[-1].content
-        )
 
-    print(f"Case ID: {caseid}")
+
+    #     chat_ctx.messages[-1].content = (
+    #         "Context:\n" + Technical + "\n\nUser Question: " + chat_ctx.messages[-1].content
+    #     )
+
+    # print(f"Case ID: {caseid}")
 
     initial_ctx = llm.ChatContext().append(
         role="system",
